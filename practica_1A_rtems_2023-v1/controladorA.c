c6c5f159a1fa0f305f8daf831eeea4e27e80c48c
Cosmin Octavian Petre Carastoian <cosminoctavianpetre@gmail.com>
2022-11-03 14:46:25 +0100

    software module, section A done

--- practica_1A_rtems_2023-v1/controladorA.c
+++ practica_1A_rtems_2023-v1/controladorA.c
@@ -29,15 +29,19 @@
 void diffTime(struct timespec end, struct timespec start, struct timespec *diff);
 void addTime(struct timespec end, struct timespec start, struct timespec *add);
 int compTime(struct timespec t1, struct timespec t2);             
-double getClock();
 
 
 //-------------------------------------
 //-  Constants
 //-------------------------------------
-#define MSG_LEN     9
-#define SLAVE_ADDR  0x8
-#define nsec        1000000000L
+#define MSG_LEN                     9
+#define SLAVE_ADDR                  0x8
+#define MIXER_STATE_CHANGE_PERIOD   30      // mixer's state should change every this much
+
+// Cyclic Scheduler stuff
+#define NUM_SC                      1       // number of secondary cycles per main cycle
+#define SC_TIME                     10      // secondary cycle time length
+
 
 //-------------------------------------
 //-  Global Variables
@@ -52,7 +56,31 @@ char brake_is_active = 0;
 char mixer_is_active = 0;
 
 // times
-double mixer_last_state_change_time;
+struct timespec mixer_state_change_last_time;
+
+
+// ------------------------------------
+// Messages
+// ------------------------------------
+// Requests
+#define REQ_SPEED       "SPD: REQ\n"
+#define REQ_SLOPE       "SLP: REQ\n"
+#define REQ_GAS_SET     "GAS: SET\n"
+#define REQ_GAS_CLR     "GAS: CLR\n"
+#define REQ_BRK_SET     "BRK: SET\n"
+#define REQ_BRK_CLR     "BRK: CLR\n"
+#define REQ_MIX_SET     "MIX: SET\n"
+#define REQ_MIX_CLR     "MIX: CLR\n"
+
+// Answers
+#define ANS_ERROR       "MSG: ERR\n"
+#define ANS_SPEED       "SPD:%f\n"
+#define ANS_SLOPE_FLAT  "SLP:FLAT\n"
+#define ANS_SLOPE_DOWN  "SLP:DOWN\n"
+#define ANS_SLOPE_UP    "SLP:  UP\n"
+#define ANS_GAS_OK      "GAS:  OK\n"
+#define ANS_BRK_OK      "BRK:  OK\n"
+#define ANS_MIX_OK      "MIX:  OK\n"
 
 
 // ------------------------------------
@@ -66,13 +94,47 @@ double mixer_last_state_change_time;
     memset(BUF2, 0, MSG_LEN+1);
 
 
-// Converts a timespec to a fractional number of seconds
-double timespec_to_double(struct timespec ts)
-{
-	return (double) (ts.tv_sec + ts.tv_nsec/nsec);
-}
+// ------------------------------------
+// Function Prototypes
+// ------------------------------------
+
+/*************** Tasks ***************/
+
+// ------------------------------------
+// Task: Read Speed
+// ------------------------------------
+int task_speed();
+// ------------------------------------
+// Task: Read Slope
+// ------------------------------------
+int task_slope();
+// ------------------------------------
+// Task: On/Off Accelerator
+// ------------------------------------
+int task_gas();
+// ------------------------------------
+// Task: On/Off Brake
+// ------------------------------------
+int task_brake();
+// ------------------------------------
+// Task: On/Off Mixer
+// ------------------------------------
+int task_mixer();
+
+/********** Aux functions ************/
+
+// reads a message from UART serial module
+// blocking call
+int read_msg(int fd, char *buffer, int max_size);
+// sends a request and receives a response
+// blocking call
+void send_req_b(char *request, char *answer);
+
+/************ Scheduler **************/
+void *controller(void *arg);
 
 
+/********** Aux functions ************/
 int read_msg(int fd, char *buffer, int max_size)
 {
     char aux_buf[MSG_LEN+1];
@@ -107,12 +169,13 @@ int read_msg(int fd, char *buffer, int max_size)
         }
     }
 
-    strncpy(buffer, "MSG: ERR\n", MSG_LEN);
+    // set error msg as answer
+    strncpy(buffer, ANS_ERROR, MSG_LEN);
     return 0;
 }
 
 
-void send_req_b(char * request, char * answer)
+void send_req_b(char *request, char *answer)
 {
 #if defined(ARDUINO)
     // use UART serial module
@@ -120,12 +183,13 @@ void send_req_b(char * request, char * answer)
     nanosleep(&time_msg, NULL);
     read_msg(fd_serie, answer, MSG_LEN);
 #else
-    //Use the simulator
+    // use the simulator
     simulator(request, answer);
 #endif
 }
 
 
+/************** Tasks ****************/
 int task_speed()
 {    
     char request[MSG_LEN+1];
@@ -134,22 +198,11 @@ int task_speed()
     CLEAR_BUFFERS(request, answer);
 
     // request speed
-    strcpy(request, "SPD: REQ\n");
+    strcpy(request, REQ_SPEED);
 
-    //TODO: refactor
-    // send_req_b(request, answer);
-#if defined(ARDUINO)
-    // use UART serial module
-    write(fd_serie, request, MSG_LEN);
-    nanosleep(&time_msg, NULL);
-    read_msg(fd_serie, answer, MSG_LEN);
-#else
-    //Use the simulator
-    simulator(request, answer);
-#endif
+    send_req_b(request, answer);
 
-    // display speed
-    if (1 == sscanf(answer, "SPD:%f\n", &speed)){
+    if (1 == sscanf(answer, ANS_SPEED, &speed)){
         displaySpeed(speed);
     }
     return 0;
@@ -162,25 +215,15 @@ int task_slope()
     char answer[MSG_LEN+1];
 
     CLEAR_BUFFERS(request, answer);
-    // request slope
-    strcpy(request, "SLP: REQ\n");
 
-    //TODO: refactor
-    // send_req_b(request, answer);
-#if defined(ARDUINO)
-    // use UART serial module
-    write(fd_serie, request, MSG_LEN);
-    nanosleep(&time_msg, NULL);
-    read_msg(fd_serie, answer, MSG_LEN);
-#else
-    //Use the simulator
-    simulator(request, answer);
-#endif
+    // request slope
+    strcpy(request, REQ_SLOPE);
+    send_req_b(request, answer);
 
     // display slope
-    if (0 == strcmp(answer, "SLP:DOWN\n")) displaySlope(-1);
-    if (0 == strcmp(answer, "SLP:FLAT\n")) displaySlope(0);
-    if (0 == strcmp(answer, "SLP:  UP\n")) displaySlope(1);
+    if (0 == strcmp(answer, ANS_SLOPE_DOWN)) displaySlope(-1);
+    if (0 == strcmp(answer, ANS_SLOPE_FLAT)) displaySlope(0);
+    if (0 == strcmp(answer, ANS_SLOPE_UP)) displaySlope(1);
 
     return 0;
 }
@@ -196,16 +239,18 @@ int task_gas()
     if (speed > 55)
         goto end;
     // at this point, the wagon should speed up
+    /* NOTE: two calls might be needed to speed up completely
+    *  one call to turn off the brake, another to turn on the gas */
     if (brake_is_active) {
-        strcpy(request, "BRK: CLR\n");
+        strcpy(request, REQ_BRK_CLR);
         brake_is_active = 0;
     } else {
-        strcpy(request, "GAS: SET\n");
+        strcpy(request, REQ_GAS_SET);
         gas_is_active = 1;
     }
 
 end:
-    // send request, get answer and display
+    // send request, get answer and display gas
     send_req_b(request, answer);
     displayGas(gas_is_active);
 
@@ -223,16 +268,18 @@ int task_brake()
     if (speed <= 55)
         goto end;
     // at this point, the wagon should slow down
+    /* NOTE: two calls might be needed to slow down completely
+    *  one call to turn off the gas, another to turn on the brake */
     if (gas_is_active) {
-        strcpy(request, "GAS: CLR\n");
+        strcpy(request, REQ_GAS_CLR);
         gas_is_active = 0;
     } else {
-        strcpy(request, "BRK: SET\n");
+        strcpy(request, REQ_BRK_SET);
         brake_is_active = 1;
     }
 
 end:
-    // send request, get answer and display
+    // send request, get answer and display brake
     send_req_b(request, answer);
     displayBrake(brake_is_active);
 
@@ -244,24 +291,34 @@ int task_mixer()
 {
     char request[MSG_LEN+1];
     char answer[MSG_LEN+1];
+    struct timespec mixer_state_change_period, time_now, delta;
+
+    mixer_state_change_period.tv_sec = MIXER_STATE_CHANGE_PERIOD;
 
     CLEAR_BUFFERS(request, answer);
 
-    //check mixing time
-    if ((getClock() - mixer_last_state_change_time) >= 30.0) {
-        switch (mixer_is_active) {
-            case 0:
-                mixer_is_active = 1;
-                strcpy(request, "MIX: SET\n");
-                break;
-            case 1:
-                mixer_is_active = 0;
-                strcpy(request, "MIX: CLR\n");
-                break;
-        }
-        mixer_last_state_change_time = getClock();
-    }
+    // get elapsed time since last state change
+    clock_gettime(CLOCK_REALTIME, &time_now);
+    diffTime(time_now, mixer_state_change_last_time, &delta);
 
+    // check if 30 seconds have passed since the last state change
+    if ( compTime(delta, mixer_state_change_period) < 0 )
+        goto end;
+    
+    switch (mixer_is_active) {
+        case 0:
+            mixer_is_active = 1;
+            strcpy(request, REQ_MIX_SET);
+            break;
+        case 1:
+            mixer_is_active = 0;
+            strcpy(request, REQ_MIX_CLR);
+            break;
+    }
+    clock_gettime(CLOCK_REALTIME, &mixer_state_change_last_time);
+
+end:
+    // send request, get answer and display mixer
     send_req_b(request, answer);
     displayMix(mixer_is_active);
 
@@ -269,16 +326,36 @@ int task_mixer()
 }
 
 
+/************ Scheduler **************/
 void *controller(void *arg)
 {
-    mixer_last_state_change_time = getClock();
+    struct timespec exe_start_time, exe_end_time, delta;
+    struct timespec sc_time, wait_time;
+    sc_time.tv_sec = SC_TIME;
+    
+    // setup initial times
+    clock_gettime(CLOCK_REALTIME, &exe_start_time);
+    clock_gettime(CLOCK_REALTIME, &mixer_state_change_last_time);
 
     while(1) {
+        // execute tasks
         task_speed();
         task_slope();
         task_gas();
         task_brake();
         task_mixer();
+
+        clock_gettime(CLOCK_REALTIME, &exe_end_time);
+        diffTime(exe_end_time, exe_start_time, &delta);
+
+        // panic: execution took too long
+        if (compTime(sc_time, delta) < 0)
+            exit(-1);
+
+        // wait till next secondary cycle
+        diffTime(sc_time, delta, &wait_time);
+        nanosleep(&wait_time, NULL);
+        addTime(exe_start_time, sc_time, &exe_start_time);
     }
 }
 
@@ -304,8 +381,7 @@ rtems_task Init (rtems_task_argument ignored)
 
 #if defined(ARDUINO)
     /* Open serial port */
-    //TODO: check that this is the right device name
-    char serial_dev[]="/dev/com1";
+    char serial_dev[] = "/dev/com1";
     fd_serie = open(serial_dev, O_RDWR);
     if (fd_serie < 0) {
         printf("open: error opening serial %s\n", serial_dev);
